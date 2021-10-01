6fb6d9523c0675e2d7df9f89fb0757198ace13a4
randel <randelfreitas@gmail.com>
2021-10-01 16:10:11 -0300

    Bug numero de paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pdf';
+  @ViewChild('content', {static: false}) el!: ElementRef;
+
+  public x = "Barraca Sorriso do Sol";
+
+  makePDF(){
+    let pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("demo.pdf");
+      }
+    });
+  }
 }
